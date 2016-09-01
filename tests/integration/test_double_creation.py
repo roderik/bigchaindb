84963595f58a182f38ce585a2a6d90b5af55102d
ryan <hendersonry@gmail.com>
2016-09-01 17:29:28 +0200

    fast double creation

--- tests/integration/test_double_creation.py
+++ tests/integration/test_double_creation.py
@@ -3,6 +3,17 @@ import pytest
 
 
 @pytest.mark.usefixtures('processes')
+def test_fast_double_create(b, user_vk):
+    tx = b.create_transaction(b.me, user_vk, None, 'CREATE')
+    tx = b.sign_transaction(tx, b.me_private)
+
+    b.write_transaction(tx)
+    b.write_transaction(tx)
+
+    assert b.get_transaction(tx['id']) == tx
+
+
+@pytest.mark.usefixtures('processes')
 def test_double_create(b, user_vk):
     tx = b.create_transaction(b.me, user_vk, None, 'CREATE')
     tx = b.sign_transaction(tx, b.me_private)
