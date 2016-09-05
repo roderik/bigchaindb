fd6eb77e8201c740505fc0844992f212f394868f
ryan <hendersonry@gmail.com>
2016-09-05 11:13:25 +0200

    test cleanup

--- tests/integration/test_double_creation.py
+++ tests/integration/test_double_creation.py
@@ -1,5 +1,6 @@
 import time
 import pytest
+import rethinkdb as r
 
 
 @pytest.mark.usefixtures('processes')
@@ -7,10 +8,20 @@ def test_fast_double_create(b, user_vk):
     tx = b.create_transaction(b.me, user_vk, None, 'CREATE')
     tx = b.sign_transaction(tx, b.me_private)
 
+    # write everything fast
     b.write_transaction(tx)
     b.write_transaction(tx)
 
-    assert b.get_transaction(tx['id']) == tx
+    time.sleep(2)
+    tx_returned = b.get_transaction(tx['id'])
+    tx.pop('assignee')
+
+    # test that the tx can be queried
+    assert tx_returned == tx
+    # test the transaction appears only once
+    assert len(list(r.table('bigchain')
+                    .get_all(tx['id'], index='transaction_id')
+                    .run(b.conn))) == 1
 
 
 @pytest.mark.usefixtures('processes')
@@ -22,4 +33,12 @@ def test_double_create(b, user_vk):
     time.sleep(2)
     b.write_transaction(tx)
     time.sleep(2)
-    b.get_transaction(tx['id'])
+    tx_returned = b.get_transaction(tx['id'])
+    tx.pop('assignee')
+
+    # test that the tx can be queried
+    assert tx_returned == tx
+    # test the transaction appears only once
+    assert len(list(r.table('bigchain')
+                    .get_all(tx['id'], index='transaction_id')
+                    .run(b.conn))) == 1
